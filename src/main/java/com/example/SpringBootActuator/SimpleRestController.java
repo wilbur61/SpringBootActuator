b746cfaf1b57a26edc7026f75c323ccd9ff00621
wilbur61 <wroales@perscholas.org>
2022-07-21 09:58:09 -0400

    Added my project

---
+++ src/main/java/com/example/SpringBootActuator/SimpleRestController.java
@@ -0,0 +1,13 @@
+package com.example.SpringBootActuator;
+
+import java.util.Date;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+ 
+@RestController
+public class SimpleRestController {
+    @GetMapping("/example")
+    public String example() {
+        return "Hello User !! " + new Date();
+    }
+}
